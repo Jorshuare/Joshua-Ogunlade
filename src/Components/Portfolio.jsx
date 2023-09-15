3767ba37a6bfeb1d0f9ba84294ee44fac5d3a034
Joshua Ogunlade <66354839+Jorshuare@users.noreply.github.com>
2023-09-15 18:43:21 +0000

    trying to find a suitable picture for my header

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/laptop-with-book.jpg";
+import image from "../images/modern-office.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
