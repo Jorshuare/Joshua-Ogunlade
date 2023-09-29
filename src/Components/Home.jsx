e502f6b0a05bd5b2f066e707b406959f0b34cd1b
Joshua Ogunlade <66354839+Jorshuare@users.noreply.github.com>
2023-09-29 08:52:41 +0000

    Corrected a major issue

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/modern-office-desk-composition-with-technological-device.jpg";
+import image from "../images/white-back-laptop.jpg";
 
 const imageAltText = "Joshua Ogunlade - Computer";
 const Home = ({ name, title }) => {
